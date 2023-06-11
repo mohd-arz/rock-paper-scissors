ff87c82f79d1c35fd965f946817ae4d1051e81b6
mohd-arz <mohammedarsh75@gmail.com>
2023-06-11 22:35:31 +0530

    Completed

---
+++ script.js
@@ -0,0 +1,115 @@
+//Created on 10-06-2023
+//lastly updated on 11-06-2023
+
+//Creating reference for needed element in DOM
+let statusDisplay=document.querySelector('.status');
+let userWinCountDisplay=document.querySelector('.userWinCount');
+let pcWinCountDisplay=document.querySelector('.pcWinCount');
+let userGuessUpdate=document.querySelector('.playerGuess');
+let pcGuessUpdate=document.querySelector('.computerGuess');
+let reset=document.querySelector('.reset');
+
+//Win Count declaration
+let userWinCount=0;
+let pcWinCount=0;
+
+//Selecting all buttons and add event listener for every individual button
+const buttons=document.querySelectorAll('button')
+buttons.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        game(button.className,playGround,computerGuess) //calling the game() with two callbacks.
+    })
+})
+
+//Getting Computer's Guess
+let computerGuess=function getComputerGuess(){
+    const values=["Rock","Paper","Scissors"];
+    return values[Math.floor(Math.random()*3)]
+}
+
+//Here, Where the game is actually played.
+function playGround(value,computerGuess){  //computerGuess is a callback from game()
+    let status="";
+    cGuess=computerGuess(); //getting computer's guess.
+    uGuess=value;
+
+    userGuessUpdate.textContent = `Your Guess: ${uGuess}`;
+    pcGuessUpdate.textContent = `Computer Guess: ${cGuess}`;
+
+    //I intentionally wrote Switch for this.
+    switch(uGuess){
+        case (cGuess):
+            status=`It's a Tie ! You both Chose ${uGuess}`;
+            break;
+
+        case ("Rock"):
+            switch(cGuess){
+                case "Paper":
+                    pcWinCount++;
+                    status=`${uGuess} lost Against ${cGuess}`;
+                    break;
+                case "Scissors":
+                    userWinCount++;
+                    status=`${uGuess} win Against ${cGuess}`
+                    break;
+            } 
+        break;
+
+        case ("Paper"):
+            switch(cGuess){
+                case "Scissors":
+                    pcWinCount++;
+                    status=`${uGuess} lost Against ${cGuess}`;
+                    break;
+                case "Rock":
+                    userWinCount++;
+                    status=`${uGuess} win Against ${cGuess}`
+                    break;
+            }
+        break
+
+        case ("Scissors"):
+            switch(cGuess){
+                case "Rock":
+                    pcWinCount++;
+                    status=`${uGuess} lost Against ${cGuess}`;
+                    break;
+                case "Paper":
+                    userWinCount++;
+                    status=`${uGuess} win Against ${cGuess}`
+                    break;
+            }
+        break;       
+    }
+    return status;
+}
+
+//Running and Showing the status of the Game
+function game(guess,playGroundCallback,computerGuessCallback){
+        statusDisplay.textContent=playGroundCallback(guess,computerGuessCallback);   
+        userWinCountDisplay.textContent=`Your Win count : ${userWinCount}`;
+        pcWinCountDisplay.textContent= `Computer's Win count : ${pcWinCount}`;
+
+    //Terminating after 5 rounds    
+    if(userWinCount === 5 || pcWinCount ===5){
+        document.querySelector('.Rock').disabled=true;         //
+        document.querySelector('.Paper').disabled=true;        //Disabling the buttons
+        document.querySelector('.Scissors').disabled=true;     //
+        let whoWon= (userWinCount===5)? "Hurray ! You WON." : "OOPS ! Computer WON."
+        statusDisplay.textContent=whoWon;
+        statusDisplay.style.textDecorationLine="underline";
+        resetFunction();
+    }
+}
+//Reset function
+function resetFunction(){
+    let resetButton=document.createElement('button');
+    resetButton.textContent="Reset"
+    reset.append(resetButton)
+    resetButton.addEventListener('click',()=>{
+        reset.removeChild(resetButton);
+        window.location.reload(); //reloads the entire page.
+    })
+}
+
+//I just learned about callbacks so really wanted to try them out.
